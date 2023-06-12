a5aa5f080710f9f5978d61108552c6eb6d56ccea
thetechnerdrohit <thetechnerdrohit@gmail.com>
2023-06-13 02:04:28 +0530

    added config

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const config = {
     defaultLocale: "en",
   },
   images: {
-    domains: ["res.cloudinary.com", "drive.google.com", "thetechnerdrohit2.netlify.app"],
+    domains: ["res.cloudinary.com"],
   },
 };
 export default config;
